aa49db39023e2c6780020058286b5b6a1277b733
kavyareddy <kavya.lfl66@gmail.com>
2019-10-11 11:16:20 -0500

    added mul

--- components/subtracter/subtracter.js
+++ components/subtracter/subtracter.js
@@ -1,5 +1,5 @@
 const subtract = (x, y, z) => { return x - y - z}
-
+const multiply = (x, y, z) => { return x * y * z}
 const subtracter = new Vue({
   el: '#subtracter',
   data: {
@@ -13,6 +13,14 @@ const subtracter = new Vue({
       const j = parseInt(this.second)
       const k = parseInt(this.third)
       return `The difference is ${subtract(i, j, k)}.`
+    }
+  },
+  computed: {
+    multiplication: function () {
+      const t = parseInt(this.first)
+      const s = parseInt(this.second)
+      const u = parseInt(this.third)
+      return `The multiplication is ${multiply(t, s, u)}.`
     }
   }
 })
