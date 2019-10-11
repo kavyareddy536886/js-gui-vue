36443fc1cc7d6673f8070968b484e868e9e7cada
kavyareddy <kavya.lfl66@gmail.com>
2019-10-11 10:48:26 -0500

    added z to substract

--- components/subtracter/subtracter.js
+++ components/subtracter/subtracter.js
@@ -1,16 +1,18 @@
-const subtract = (x, y) => { return x - y }
+const subtract = (x, y, z) => { return x - y - z}
 
 const subtracter = new Vue({
   el: '#subtracter',
   data: {
     first: 6,
-    second: 2
+    second: 2,
+    third: 7
   },
   computed: {
     difference: function () {
       const i = parseInt(this.first)
       const j = parseInt(this.second)
-      return `The difference is ${subtract(i, j)}.`
+      const z = parseInt(this.third)
+      return `The difference is ${subtract(i, j, k)}.`
     }
   }
 })
