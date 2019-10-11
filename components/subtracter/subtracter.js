122be31a102f7459d2c441e34139c34027758172
kavyareddy <kavya.lfl66@gmail.com>
2019-10-11 13:07:20 -0500

    sjdhsj

--- components/subtracter/subtracter.js
+++ components/subtracter/subtracter.js
@@ -1,24 +1,18 @@
-const subtract = (x, y, z) => { return x - y - z}
-const multiply = (x, y, z) => { return x * y * z}
+
+const multiply = (x, y) => { return x * y }
 const subtracter = new Vue({
   el: '#subtracter',
   data: {
     first: 6,
-    second: 2,
-    third: 4
+    second: 2
+   
   },
   computed: {
-    difference: function () {
-      const i = parseInt(this.first)
-      const j = parseInt(this.second)
-      const k = parseInt(this.third)
-      return `The difference is ${subtract(i, j, k)}.`
-    },
+   
     multiplication: function () {
       const i = parseInt(this.first)
       const j = parseInt(this.second)
-      const k = parseInt(this.third)
-      return `The multiplication is ${multiply(i, j, k)}.`
+      return `The multiplication is ${multiply(i, j)}.`
     }
   }
   
